449fba6f5dc2b39a2b06f9763604a68175026245
lmachens <leon.machens@googlemail.com>
2020-07-21 16:46:11 +0200

    Add quantity selector

---
+++ src/components/quantitySelector/quantitySelector.stories.js
@@ -0,0 +1,6 @@
+import "./quantitySelector.css";
+import quantitySelector from "./quantitySelector.html";
+
+export default { title: "Quantity Selector" };
+
+export const coffeeSelector = () => quantitySelector;
