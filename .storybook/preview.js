7735d34e930e7573a67c44ea110c78e6bd068889
lmachens <leon.machens@googlemail.com>
2020-07-22 11:23:54 +0200

    Add global css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { addDecorator, addParameters } from "@storybook/html"; // <- or your sto
 import { withA11y } from "@storybook/addon-a11y";
 import "@storybook/addon-console";
 import centered from "@storybook/addon-centered/html";
+import "../src/global.css";
 
 addDecorator(centered);
 addDecorator(withA11y);
