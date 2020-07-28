ff8abe0da6182b8095c71777b6387a83c0f8fb23
lmachens <leon.machens@googlemail.com>
2020-07-28 11:50:59 +0200

    Remove unused css rule

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -3,6 +3,12 @@ import "./button.css";
 export default { title: "Button" };
 
 export const addToCart = () => {
+  document.createElement("button");
+  document.createElement("button");
+  document.createElement("button");
+  document.createElement("button");
+  document.createElement("button");
+  document.createElement("button");
   const button = document.createElement("button");
   button.classList.add("btn");
   button.innerHTML = "Add to cart";
