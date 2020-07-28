e45b73e39adaff557c4eaa31d5f7a7a3d220b570
lmachens <leon.machens@googlemail.com>
2020-07-28 14:18:35 +0200

    Create button helper

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -2,10 +2,15 @@ import "./button.css";
 
 export default { title: "Button" };
 
-export const addToCart = () => {
+function createButton(buttonText) {
   const button = document.createElement("button");
-  button.classList.add("btn");
-  button.innerHTML = "Add to cart";
+  button.className = "btn";
+  const text = document.createTextNode(buttonText);
+  button.append(text);
+  return button;
+}
 
+export const addToCart = () => {
+  const button = createButton("Add to cart");
   return button;
 };
