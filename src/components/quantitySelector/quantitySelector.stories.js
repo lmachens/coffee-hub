4bc8e8e051c93d9fe308aa2f36cf17b56586f71e
lmachens <leon.machens@googlemail.com>
2020-07-29 11:00:25 +0200

    Add quantitySelector.js

--- src/components/quantitySelector/quantitySelector.stories.js
+++ src/components/quantitySelector/quantitySelector.stories.js
@@ -1,31 +1,35 @@
 import "./quantitySelector.css";
-import quantitySelector from "./quantitySelector.html";
-import { useEffect } from "@storybook/client-api";
+import { createQuantitySelector } from "./quantitySelector";
 
 export default { title: "Quantity Selector" };
 
 export const coffeeSelector = () => {
-  useEffect(() => {
-    // Select buttons here
-    const minusButton = document.querySelector(".quantitySelector__minus");
-    const plusButton = document.querySelector(".quantitySelector__plus");
-    const resultElement = document.querySelector(".quantitySelector__result");
-
-    minusButton.addEventListener("click", () => {
-      const oldResult = Number(resultElement.innerHTML);
-      if (oldResult === 2) {
-        minusButton.disabled = true;
-      }
-      resultElement.innerHTML = oldResult - 1;
-    });
-    plusButton.addEventListener("click", () => {
-      const oldResult = Number(resultElement.innerHTML);
-      if (oldResult === 1) {
-        minusButton.disabled = false;
-      }
-      resultElement.innerHTML = oldResult + 1;
-    });
-  });
+  const quantitySelector = createQuantitySelector();
 
   return quantitySelector;
 };
+// export const coffeeSelector = () => {
+//   useEffect(() => {
+//     // Select buttons here
+//     const minusButton = document.querySelector(".quantitySelector__minus");
+//     const plusButton = document.querySelector(".quantitySelector__plus");
+//     const resultElement = document.querySelector(".quantitySelector__result");
+
+//     minusButton.addEventListener("click", () => {
+//       const oldResult = Number(resultElement.innerHTML);
+//       if (oldResult === 2) {
+//         minusButton.disabled = true;
+//       }
+//       resultElement.innerHTML = oldResult - 1;
+//     });
+//     plusButton.addEventListener("click", () => {
+//       const oldResult = Number(resultElement.innerHTML);
+//       if (oldResult === 1) {
+//         minusButton.disabled = false;
+//       }
+//       resultElement.innerHTML = oldResult + 1;
+//     });
+//   });
+
+//   return quantitySelector;
+// };
