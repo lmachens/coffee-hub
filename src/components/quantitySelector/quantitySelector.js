a055532d395a3e532330ddd2b04d0f9c5897be0d
lmachens <leon.machens@googlemail.com>
2020-07-29 11:07:35 +0200

    Add buttons and result

--- src/components/quantitySelector/quantitySelector.js
+++ src/components/quantitySelector/quantitySelector.js
@@ -7,8 +7,21 @@ export const createQuantitySelector = () => {
   const minusButton = document.createElement("button");
   minusButton.className = "quantitySelector__minus";
   minusButton.innerHTML = "-";
+  minusButton.disabled = true;
+
+  // Result
+  const resultElement = document.createElement("div");
+  resultElement.className = "quantitySelector__result";
+  resultElement.innerText = "1";
+
+  // Second button (Plus)
+  const plusButton = document.createElement("button");
+  plusButton.className = "quantitySelector__plus";
+  plusButton.innerHTML = "+";
 
   quantitySelector.append(minusButton);
+  quantitySelector.append(resultElement);
+  quantitySelector.append(plusButton);
 
   return quantitySelector;
 };
