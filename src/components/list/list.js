e76bdb0e3bc26741ade209c4dba1175a80a38061
marwinburesch <marwin@posteo.de>
2020-07-30 17:29:42 +0200

    Add arrow right and span

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -5,10 +5,12 @@ import cappuccinoSrc from "../../assets/cappuccino.svg";
 import macchiatoSrc from "../../assets/macchiato.svg";
 import mochaSrc from "../../assets/mocha.svg";
 import latteSrc from "../../assets/latte.svg";
+import arrowSrc from "../../assets/right.svg";
 // import syntax: https://javascript.info/import-export
 // "{ createElement }" syntax destructuring, noch nicht verstehen
 
 export function createList() {
+  // assign array X to variable "coffees"
   const coffees = [
     { name: "Espresso", imgSrc: espressoSrc, imgClass: "list-item_icon" },
     { name: "Cappuchino", imgSrc: cappuccinoSrc, imgClass: "list-item_icon" },
@@ -17,21 +19,30 @@ export function createList() {
     { name: "Latte", imgSrc: latteSrc, imgClass: "list-item_icon" },
   ];
 
-  // assign array X to variable "coffees"
+  // create parent element "ul" with class "list"
   const list = createElement("ul");
+  list.className = "list";
 
   // forEach is an array method: https://javascript.info/array-methods
   // "(parameter) => {}" is a callback function
   // it gets called for each "thing" in the array
   // arrow functions: https://javascript.info/arrow-functions-basics
   coffees.forEach((coffee) => {
-    const listItem = createElement("li", { innerText: coffee.name });
-    const img = createElement("img", {
+    const listItem = createElement("li");
+    const coffeeImg = createElement("img", {
       src: coffee.imgSrc,
     });
+    listItem.append(coffeeImg);
+    coffeeImg.className = coffee.imgClass;
 
-    img.className = coffee.imgClass;
-    listItem.prepend(img);
+    const span = createElement("span", { innerText: coffee.name });
+    listItem.append(span);
+
+    const arrowIcon = createElement("img", {
+      src: arrowSrc,
+    });
+    listItem.append(arrowIcon);
+
     list.append(listItem);
   });
 
