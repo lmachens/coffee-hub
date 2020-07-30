4765ff12b5953440f14b47f44da8afc4eb98276d
marwinburesch <marwin@posteo.de>
2020-07-30 17:30:00 +0200

    Add fullWidthContainer

--- src/components/list/list.stories.js
+++ src/components/list/list.stories.js
@@ -1,9 +1,13 @@
 import "./list.css";
 import { createList } from "./list";
+import { createElement } from "../../utils/elements";
 
 export default { title: "List" };
 
 export const coffeeList = () => {
+  const fullWidthContainer = createElement("div", { className: "fullWidth" });
   const list = createList();
-  return list;
+  fullWidthContainer.append(list);
+
+  return fullWidthContainer;
 };
